d2843da848db9371870a8d5e4b664213e3d60f83
Gabor Szathmari <gszathmari@gmail.com>
2015-07-22 22:56:24 +0100

    Store mirror data in file instead of memory; remove external script differ

--- buster.py
+++ buster.py
@@ -4,6 +4,7 @@ import requests
 import argparse
 import sys
 import difflib
+import shelve
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
@@ -11,62 +12,70 @@ TIMEOUT = 5
 # Fake user agent because of mischevious mirrors
 HEADERS = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12'}
 
+class Mirrors:
+    def __init__(self, filename):
+        self.data = shelve.open(filename)
+
+    def __getitem__(self, url):
+        try:
+            return self.data[url]
+        except KeyError:
+            pass
+
+    def close(self):
+        self.data.close()
+
+    # Retrieve website mirrors
+    def populate(self, urls):
+        counter = 1
+        for url in urls:
+            print("-> Retrieving mirror (%s/%s): %s" % (counter, len(mirror_urls), url))
+            # Create website Object
+            mirror = Website(url)
+            # Do not add if retrieval was not successful
+            if mirror.has_errors() is None:
+                self.data[url] = mirror
+            else:
+                print("There was an error while retrieving %s, skipping ..." % url)
+                if args.debug:
+                    print("Message: %s\n" % mirror.has_errors())
+            counter += 1
+
 # Object to store website data
 class Website:
     def __init__(self, url):
-        u = urlparse(url)
-        self.url = u.geturl()
-        self.scheme = u.scheme
+        url_object = urlparse(url)
+        self.url = url_object.geturl()
+        self.scheme = url_object.scheme
         self.error = None
+        self.inline_scripts = []
+        self.external_script_urls = []
         try:
             r = requests.get(self.url, timeout=TIMEOUT, headers=HEADERS)
             if r.status_code == 200:
-                self.soup = BeautifulSoup(r.text, 'html.parser')
+                soup = BeautifulSoup(r.text, 'html.parser')
+                # Parse HTML for <script> tags
+                for tag in soup.find_all("script"):
+                    # If tag is <script src="">
+                    if tag.get("src"):
+                        url = urlparse(tag.get("src"), self.scheme)
+                        self.external_script_urls.append(url)
+                    # If tag is inline script
+                    else:
+                        self.inline_scripts.append(str(tag))
             else:
                 self.error = Error("Website did not respond with HTTP 200")
         except Exception as e:
             self.error = e
 
-    # To check if retrieval was successful
     def has_errors(self):
         return self.error
 
-    # Parse HTML to extract inline <script> tags
     def get_inline_scripts(self):
-        results = []
-        for tag in self.soup.find_all("script"):
-            # This is for filtering out external scripts
-            if tag.get("src") is None:
-                results.append(str(tag))
-        return sorted(results)
+        return sorted(self.inline_scripts)
 
-    # Parse HTML to extract URLs pointing to external scripts
     def get_external_script_urls(self):
-        results = []
-        for tag in self.soup.find_all("script"):
-            # We filter out inline scripts
-            if tag.get("src"):
-                url = urlparse(tag.get("src"), self.scheme)
-                results.append(url)
-        return sorted(results)
-
-# Retrieve website mirrors
-def retrieve_mirrors(urls):
-    mirrors = []
-    counter = 1
-    for url in urls:
-        print("-> Retrieving mirror (%s/%s): %s" % (counter, len(args.mirrors), url))
-        # Create website Object
-        mirror = Website(url)
-        # Do not add if retrieval was not successful
-        if mirror.has_errors() is None:
-            mirrors.append(mirror)
-        else:
-            print("There was an error while retrieving %s, skipping ..." % url)
-            if args.debug:
-                print("Message: %s\n" % mirror.has_errors())
-        counter += 1
-    return mirrors
+        return sorted(self.external_script_urls)
 
 # Retrieve original website for comparison
 def retrieve_original(url):
@@ -98,71 +107,54 @@ def diff_external_script_urls(original, mirror):
             results.append(mirror_url)
     return results
 
-# Retrieve external scripts and compare their contents
-def diff_external_script_contents(original, mirror):
-    results = []
-    # Get all script URLs from original website
-    original_script_urls = [url.geturl() for url in original.get_external_script_urls()]
-    # Get all script URLs from mirror
-    mirror_script_urls = mirror.get_external_script_urls()
-    # Find script URLs that are in the original and the mirror site
-    for mirror_url in mirror_script_urls:
-        for original_url in original_script_urls:
-            # If matching script URL is found, retrieve both for comparison
-            if original_url.endswith(mirror_url.path):
-                try:
-                    original_script = requests.get(original_url, timeout=TIMEOUT, headers=HEADERS)
-                    mirror_script = requests.get(mirror_url.geturl(), timeout=TIMEOUT, headers=HEADERS)
-                    # If retrieval was successul, compare if they match
-                    if original_script.status_code == 200 and mirror_script.status_code == 200:
-                        # If script does not match, add to results
-                        if len(original_script.text) != len(mirror_script.text):
-                            result = {
-                                'original_url': original_url,
-                                'mirror_url': mirror_url.geturl(),
-                                'original_script': original_script.text,
-                                'mirror_script': mirror_script.text
-                            }
-                            results.append(result)
-                # Handle requests library exceptions
-                except Exception as e:
-                    if args.debug:
-                        print("-> Error: Could not check difference between %s and %s\n" % (url1, url2))
-                        print(e)
-                    else:
-                        pass
-    return results
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compare JavaScript assets between a selected website and its mirrors")
     groupMandatory = parser.add_argument_group('available mirror tests')
+    groupDataFile = parser.add_argument_group('data file')
     parser.add_argument('-u', '--url', help='original website URL', required=True)
+    parser.add_argument('-m', '--mirror-list', help='supply mirror URLs from file', metavar='file')
     parser.add_argument('mirrors', help='list of mirror website URLs', metavar='mirror', nargs='*')
     groupMandatory.add_argument('-i', '--inline', help='compare inline scripts between <script></script> tags', action='store_true')
     groupMandatory.add_argument('-e', '--external', help='compare list of URLs in <script src="">', action='store_true')
-    groupMandatory.add_argument('-a', '--assets', help='compare contents of matching external scripts', action='store_true')
     groupMandatory.add_argument('-o', '--output', help='print test statistics into CSV file', metavar='file')
+    groupDataFile.add_argument('-f', '--file', help='data file to store responses from mirrors', default='mirrors.dat')
+    groupDataFile.add_argument('-n', '--nocheck', help='use data file instead of the Internet', action='store_true')
     parser.add_argument('-v', '--verbose', help='print contents of results', action='store_true')
     parser.add_argument('-d', '--debug', help='debug mode', action='store_true')
 
     args = parser.parse_args()
-    # Retrieve original and mirror websites
+
+    if args.mirror_list:
+        mirror_urls = []
+        with open(args.mirror_list, 'r') as f:
+            for line in f:
+                mirror_urls.append(line.rstrip())
+    else:
+        mirror_urls = args.mirrors
+
+    # Retrieve original website
     original = retrieve_original(args.url)
-    mirrors = retrieve_mirrors(args.mirrors)
+
+    # Load mirrors data file
+    mirrors = Mirrors(args.file)
+
+    if not args.nocheck:
+        # Populate mirrors data file
+        mirrors.populate(mirror_urls)
 
     # Generate CSV file with stats
     if args.output:
         separator = ','
         with open(args.output, 'w') as file:
-            header = ["Original", "Mirror", "Additional Inline Scripts", "Additional External Scripts", "Differing External Scripts"]
+            header = ["Original", "Mirror", "Additional Inline Scripts", "Additional External Scripts"]
             file.write("%s\n" % separator.join(header))
-            for mirror in mirrors:
+            for url in mirror_urls:
+                mirror = Mirror.get(url)
                 line = []
                 line.append(original.url)
                 line.append(mirror.url)
                 line.append(len(diff_inline_scripts(original, mirror)))
                 line.append(len(diff_external_script_urls(original, mirror)))
-                line.append(len(diff_external_script_contents(original, mirror)))
                 output = map(str, line)
                 file.write("%s\n" % separator.join(output))
         file.close()
@@ -171,7 +163,12 @@ if __name__ == "__main__":
     if args.inline:
         print("\nCOMPARING INLINE SCRIPTS WITH ORIGINAL SITE")
         print('=' * 60)
-        for mirror in mirrors:
+        for url in mirror_urls:
+            mirror = mirrors[url]
+            # Skip if URL does not exist in data file
+            if not mirror:
+                print("-> %s does not exist in data file, skipping ...\n" % url)
+                break
             print("\n%s <=> %s" % (original.url, mirror.url))
             print('-' * 60)
             results = diff_inline_scripts(original, mirror)
@@ -188,7 +185,12 @@ if __name__ == "__main__":
     if args.external:
         print("\nDIFFERENCES IN LIST OF EXTERNAL SCRIPTS WITH ORIGINAL SITE")
         print('=' * 60)
-        for mirror in mirrors:
+        for url in mirror_urls:
+            mirror = mirrors[url]
+            # Skip if URL does not exist in data file
+            if not mirror:
+                print("-> %s does not exist in data file, skipping ...\n" % url)
+                break
             print("\n%s <=> %s" % (original.url, mirror.url))
             print('-' * 60)
             results = diff_external_script_urls(original, mirror)
@@ -201,22 +203,5 @@ if __name__ == "__main__":
             else:
                 print("-> Phew! No difference is found, list of external scripts match\n")
 
-    if args.assets:
-        print("\nDIFFERENCES IN CONTENTS OF MATCHING EXTERNAL SCRIPTS")
-        print('=' * 60)
-        for mirror in mirrors:
-            print("\n%s <=> %s" % (original.url, mirror.url))
-            print('-' * 60)
-            scripts = diff_external_script_contents(original, mirror)
-            if len(scripts) > 0:
-                for script in scripts:
-                    # Use difflib to generate fancy results
-                    result = []
-                    print("!!! Difference found in %s and %s\n" % (script['original_url'], script['mirror_url']))
-                    for line in difflib.unified_diff(script['original_script'].split("\n"), script['mirror_script'].split("\n"), fromfile=script['original_script'], tofile=script['mirror_script']):
-                        result.append(line[:75] + (line[75:] and ".."))
-                        print("\n".join(result))
-            else:
-                print("-> Phew! No differences are found in external scripts\n")
-
+mirrors.close()
 print("Done.\n")
